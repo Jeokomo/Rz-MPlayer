0fe4c383393c059ae697d19b67d675cad237cd79
Jeokomo <darksourcemarshal@mail.ru>
2017-03-05 17:41:22 +1000

    Basis of project

---
+++ Rz.MPlayer/Rz.MPlayer/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+
+namespace Rz.MPlayer.ViewModel
+{
+
+   class MainWindowViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindowViewModel class.
+        /// </summary>
+       
+    }
+}
